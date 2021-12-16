02dde197a08501af95fd48d3aaaf57320951e842
Mateus Gomes <mateus10gomes@gmail.com>
2021-12-15 21:34:57 -0300

    Card Commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,20 @@
-import '../styles/home.scss'
 import { Header } from "../components/Header"
 import { ButtonWhatsapp } from "../components/ButtonWhatsapp"
 import { Footer } from "../components/Footer"
+import { Card } from "../components/Card"
+
+import '../styles/main.scss'
 
 export function Home(){
     return(
         <div>
             <Header/>
             <main>
-               
-            
+                <img className="background-img" src="https://www.vojtechruzicka.com/static/d24ceaad89c616f9ef8dfca453e2786e/8e1fc/constructor-functions.jpg" alt="" />
+                
+                <div className="parent">
+                </div> 
+                <Card/>
                 <ButtonWhatsapp/>
             </main>
             <Footer/>
